ca4c7c4288202fc715a385d02b8df22d1b6c0564
wickit <45633047+wickit7@users.noreply.github.com>
2023-03-07 17:08:52 +0100

    Add files via upload

---
+++ tutorial/tutorial_3/tutorial_3_create.bat
@@ -0,0 +1,9 @@
+@echo off
+chcp 65001
+
+rem create datamodel with JSON
+"C:\Program Files\ArcGIS\Pro\bin\Python\envs\arcgispro-py3\python.exe" "..\..\create_db_model.py" "tutorial_3_create.json"
+
+pause
+
+
